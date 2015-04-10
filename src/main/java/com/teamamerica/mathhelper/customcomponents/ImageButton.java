b0fdb21dd087e2884f96f21233462da42fedbd5d
creid1 <creid1@student.framingham.edu>
2015-04-10 15:40:25 -0400

    Added Custom Image Components
    custom image components for labels, jbuttons, radiobuttons, and
    checkboxes

---
+++ src/main/java/com/teamamerica/mathhelper/customcomponents/ImageButton.java
@@ -0,0 +1,48 @@
+package com.teamamerica.mathhelper.customcomponents;
+
+import com.teamamerica.mathhelper.configurations.ConfigDirectory;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.net.URL;
+
+/**
+ * Created by Christina on 4/10/2015.
+ */
+public class ImageButton extends JButton {
+
+    /** Filename of the image to be used as the button's icon. */
+    private String fileName;
+    /** The width of the button */
+    private int width;
+    /** The height of the button. */
+    private int height;
+
+    public ImageButton(String fileName, int width, int height){
+        this.fileName = fileName;
+        this.width = width;
+        this.height = height;
+        createButton();
+    }
+
+    /**
+     * Creates the button according to the fields set by the constructor.
+     */
+    private void createButton(){
+        this.setIcon(getImageIcon(fileName));
+        this.setPreferredSize(new Dimension(width, height));
+        this.setMaximumSize(new Dimension(width, height));
+        this.setFocusPainted(false);
+        this.setRolloverEnabled(false);
+        this.setOpaque(false);
+        this.setContentAreaFilled(false);
+        this.setBorderPainted(false);
+        this.setBorder(BorderFactory.createEmptyBorder(0,0,0,0));
+    }
+
+    public ImageIcon getImageIcon(String fileName){
+        return new ImageIcon(ConfigDirectory.getCircleImageFileFromDirectory(fileName));
+    }
+}
+
---
+++ src/main/java/com/teamamerica/mathhelper/customcomponents/ImageRadioButton.java
@@ -0,0 +1,47 @@
+package com.teamamerica.mathhelper.customcomponents;
+
+import com.teamamerica.mathhelper.configurations.ConfigDirectory;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by Christina on 4/10/2015.
+ */
+public class ImageRadioButton extends JRadioButton {
+
+
+    /** Filename of the image to be used as the button's icon. */
+    private String fileName;
+    /** The width of the button */
+    private int width;
+    /** The height of the button. */
+    private int height;
+
+    public ImageRadioButton(String fileName, int width, int height){
+        this.fileName = fileName;
+        this.width = width;
+        this.height = height;
+        createButton();
+    }
+
+    /**
+     * Creates the button according to the fields set by the constructor.
+     */
+    private void createButton(){
+        this.setIcon(getImageIcon(fileName));
+        this.setPreferredSize(new Dimension(width, height));
+        this.setMaximumSize(new Dimension(width, height));
+        this.setFocusPainted(false);
+        this.setRolloverEnabled(false);
+        this.setOpaque(false);
+        this.setContentAreaFilled(false);
+        this.setBorderPainted(false);
+        this.setBorder(BorderFactory.createEmptyBorder(0,0,0,0));
+    }
+
+    public ImageIcon getImageIcon(String fileName){
+        return new ImageIcon(ConfigDirectory.getCircleImageFileFromDirectory(fileName));
+    }
+}
+
